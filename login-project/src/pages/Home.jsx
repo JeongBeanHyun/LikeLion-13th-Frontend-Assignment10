b48c5ff3925406fed7e7b5ec2db71ced7fb45e1d
JeongBeanHyun <kelly92108@gmail.com>
2025-09-26 01:20:03 +0900

    feat: 비밀번호 변경 버튼 추가

--- login-project/src/pages/Home.jsx
+++ login-project/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import LoginForm from "../components/LoginForm";
 import LoginStatus from "../components/LoginStatus";
 import { useAuthStore } from "../stores/authStore";
@@ -5,6 +6,7 @@ import "../styles/Home.css";
 
 export default function Home() {
   const isLoggedIn = useAuthStore((s) => s.isLoggedIn);
+  const [showChangePassword, setShowChangePassword] = useState(false);
 
   return (
     <div className="home-page">
@@ -12,6 +14,11 @@ export default function Home() {
       <LoginStatus />
       {!isLoggedIn && <LoginForm />}
       {isLoggedIn && <p className="home-text">로그인 완료!</p>}
+      {isLoggedIn && !showChangePassword && (
+        <button onClick={() => setShowChangePassword(true)}>
+          비밀번호 변경
+        </button>
+      )}
     </div>
   );
 }
